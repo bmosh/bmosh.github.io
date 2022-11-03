9eabf35d8997c0d661672659153a1355bea521e5
Mosh <ram487@cornell.edu>
2022-11-03 14:48:31 -0400

    Adjusted navbar visuals, added in title-name holder for heading.

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,24 @@ function user_name() {
   if (!name) {
     return user_name();
   } else {
+    localStorage.setItem('name', name)
     return name;
   }
 }
+function changeName() {
+  myHeading.textContent = "Hello " + user_name() + "!";
+}
 
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello " + user_name() + "!";
+
+if (localStorage.name) {
+  myHeading.textContent = "Hello " + localStorage.getItem('name') + "!";
+} else {
+  myHeading.onclick = function(){ changeName() };
+}
+
 
 const topHeading = document.getElementsByClassName("navbar-title");
 
 function change_title_text() {
-  document.getElementsByClassName("navbar-title").textContent = "Rob Mosher";
 }
